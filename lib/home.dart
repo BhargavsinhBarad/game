bdd5e80e56706ba650acc5b3875abd94244e39f5
BHARGAVSINH <bhargavsinhbarad29@gmail.com>
2023-09-08 21:59:43 +0530

    commit

--- lib/home.dart
+++ lib/home.dart
@@ -8,6 +8,7 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  int animal = 0;
   @override
   Widget build(BuildContext contexty0) {
     return Scaffold(
@@ -16,147 +17,226 @@ class _HomeState extends State<Home> {
       ),
       body: Column(
         children: [
-          SingleChildScrollView(
-            scrollDirection: Axis.horizontal,
-            child: Row(
-              children: [
-                Draggable(
-                  feedback: Container(
-                    height: 100,
-                    width: 70,
-                    child: Image.asset("lib/Assets/1.jpg"),
-                  ),
-                  child: Container(
-                    height: 100,
-                    width: 70,
-                    child: Image.asset("lib/Assets/1.jpg"),
-                  ),
+          Row(
+            children: [
+              Draggable(
+                data: 1,
+                childWhenDragging: Container(),
+                feedback: Container(
+                  height: 100,
+                  width: 70,
+                  child: Image.asset("lib/Assets/1.jpg"),
                 ),
-                Draggable(
-                  feedback: Container(
-                    height: 100,
-                    width: 70,
-                    child: Image.asset("lib/Assets/2.jpg"),
-                  ),
-                  child: Container(
-                    height: 100,
-                    width: 70,
-                    child: Image.asset("lib/Assets/2.jpg"),
-                  ),
+                child: Container(
+                  height: 100,
+                  width: 70,
+                  child: Image.asset("lib/Assets/1.jpg"),
                 ),
-                Draggable(
-                  feedback: Container(
-                    height: 100,
-                    width: 70,
-                    child: Image.asset("lib/Assets/3.jpg"),
-                  ),
-                  child: Container(
-                    height: 100,
-                    width: 70,
-                    child: Image.asset("lib/Assets/3.jpg"),
-                  ),
+              ),
+              Draggable(
+                data: 2,
+                childWhenDragging: Container(),
+                feedback: Container(
+                  height: 100,
+                  width: 70,
+                  child: Image.asset("lib/Assets/2.jpg"),
                 ),
-                Draggable(
-                  feedback: Container(
-                    height: 100,
-                    width: 70,
-                    child: Image.asset("lib/Assets/4.jpg"),
-                  ),
-                  child: Container(
-                    height: 100,
-                    width: 70,
-                    child: Image.asset("lib/Assets/4.jpg"),
-                  ),
+                child: Container(
+                  height: 100,
+                  width: 70,
+                  child: Image.asset("lib/Assets/2.jpg"),
                 ),
-                Draggable(
-                  feedback: Container(
-                    height: 100,
-                    width: 70,
-                    child: Image.asset("lib/Assets/5.png"),
-                  ),
-                  child: Container(
-                    height: 100,
-                    width: 70,
-                    child: Image.asset("lib/Assets/5.png"),
-                  ),
+              ),
+              Draggable(
+                data: 3,
+                childWhenDragging: Container(),
+                feedback: Container(
+                  height: 100,
+                  width: 70,
+                  child: Image.asset("lib/Assets/3.jpg"),
                 ),
-              ],
-            ),
+                child: Container(
+                  height: 100,
+                  width: 70,
+                  child: Image.asset("lib/Assets/3.jpg"),
+                ),
+              ),
+              Draggable(
+                data: 4,
+                childWhenDragging: Container(),
+                feedback: Container(
+                  height: 100,
+                  width: 70,
+                  child: Image.asset("lib/Assets/4.jpg"),
+                ),
+                child: Container(
+                  height: 100,
+                  width: 70,
+                  child: Image.asset("lib/Assets/4.jpg"),
+                ),
+              ),
+              Draggable(
+                data: 5,
+                childWhenDragging: Container(),
+                feedback: Container(
+                  height: 100,
+                  width: 70,
+                  child: Image.asset("lib/Assets/5.png"),
+                ),
+                child: Container(
+                  height: 100,
+                  width: 70,
+                  child: Image.asset("lib/Assets/5.png"),
+                ),
+              ),
+            ],
           ),
-          SingleChildScrollView(
-            scrollDirection: Axis.horizontal,
-            child: Row(
-              children: [
-                Draggable(
-                  feedback: Container(
-                    height: 100,
-                    width: 70,
-                    child: Image.asset("lib/Assets/6.png"),
-                  ),
-                  child: Container(
-                    height: 100,
-                    width: 70,
-                    child: Image.asset("lib/Assets/6.png"),
-                  ),
+          Row(
+            children: [
+              Draggable(
+                data: 6,
+                childWhenDragging: Container(),
+                feedback: Container(
+                  height: 100,
+                  width: 70,
+                  child: Image.asset("lib/Assets/6.png"),
                 ),
-                Draggable(
-                  feedback: Container(
-                    height: 100,
-                    width: 70,
-                    child: Image.asset("lib/Assets/7.png"),
-                  ),
-                  child: Container(
-                    height: 100,
-                    width: 70,
-                    child: Image.asset("lib/Assets/7.png"),
-                  ),
+                child: Container(
+                  height: 100,
+                  width: 70,
+                  child: Image.asset("lib/Assets/6.png"),
                 ),
-                Draggable(
-                  feedback: Container(
-                    height: 100,
-                    width: 70,
-                    child: Image.asset("lib/Assets/8.png"),
-                  ),
-                  child: Container(
-                    height: 100,
-                    width: 70,
-                    child: Image.asset("lib/Assets/8.png"),
-                  ),
+              ),
+              Draggable(
+                data: 7,
+                childWhenDragging: Container(),
+                feedback: Container(
+                  height: 100,
+                  width: 70,
+                  child: Image.asset("lib/Assets/7.png"),
                 ),
-                Draggable(
-                  feedback: Container(
-                    height: 100,
-                    width: 70,
-                    child: Image.asset("lib/Assets/9.png"),
-                  ),
-                  child: Container(
-                    height: 100,
-                    width: 70,
-                    child: Image.asset("lib/Assets/9.png"),
-                  ),
+                child: Container(
+                  height: 100,
+                  width: 70,
+                  child: Image.asset("lib/Assets/7.png"),
                 ),
-                Draggable(
-                  feedback: Container(
-                    height: 100,
-                    width: 70,
-                    child: Image.asset("lib/Assets/10.png"),
-                  ),
-                  child: Container(
-                    height: 100,
-                    width: 70,
-                    child: Image.asset("lib/Assets/10.png"),
-                  ),
+              ),
+              Draggable(
+                data: 8,
+                childWhenDragging: Container(),
+                feedback: Container(
+                  height: 100,
+                  width: 70,
+                  child: Image.asset("lib/Assets/8.png"),
                 ),
-              ],
-            ),
+                child: Container(
+                  height: 100,
+                  width: 70,
+                  child: Image.asset("lib/Assets/8.png"),
+                ),
+              ),
+              Draggable(
+                data: 9,
+                childWhenDragging: Container(),
+                feedback: Container(
+                  height: 100,
+                  width: 70,
+                  child: Image.asset("lib/Assets/9.png"),
+                ),
+                child: Container(
+                  height: 100,
+                  width: 70,
+                  child: Image.asset("lib/Assets/9.png"),
+                ),
+              ),
+              Draggable(
+                data: 10,
+                childWhenDragging: Container(),
+                feedback: Container(
+                  height: 100,
+                  width: 70,
+                  child: Image.asset("lib/Assets/10.png"),
+                ),
+                child: Container(
+                  height: 100,
+                  width: 70,
+                  child: Image.asset("lib/Assets/10.png"),
+                ),
+              ),
+            ],
           ),
           Spacer(),
-          Container(
-            margin: EdgeInsets.only(bottom: 120),
-            height: 250,
-            width: 300,
-            child: Image.asset("lib/Assets/11.png"),
-          )
+          DragTarget(
+            onWillAccept: (val) {
+              return val == 1;
+            },
+            onAccept: (int val) {
+              setState(() {
+                animal = val;
+              });
+            },
+            builder: (
+              BuildContext context,
+              List<dynamic> accepted,
+              List<dynamic> rejected,
+            ) {
+              return Stack(
+                children: [
+                  Center(
+                    child: Container(
+                        child: Column(
+                      children: [
+                        (animal == 0)
+                            ? Container(
+                                margin: EdgeInsets.all(20),
+                                height: 200,
+                                width: 200,
+                                child: Image.asset(
+                                  "lib/Assets/12.jpg",
+                                  fit: BoxFit.fill,
+                                ))
+                            : (animal == 2)
+                                ? Container(
+                                    margin: EdgeInsets.all(20),
+                                    height: 200,
+                                    width: 200,
+                                    child: Image.asset(
+                                      "lib/Assets/2.jpg",
+                                      fit: BoxFit.fill,
+                                    ))
+                                : (animal == 3)
+                                    ? Container(
+                                        margin: EdgeInsets.all(20),
+                                        height: 200,
+                                        width: 200,
+                                        child: Image.asset(
+                                          "lib/Assets/3.jpg",
+                                          fit: BoxFit.fill,
+                                        ))
+                                    : Container(
+                                        margin: EdgeInsets.all(20),
+                                        height: 200,
+                                        width: 200,
+                                        child: Image.asset(
+                                          "lib/Assets/5.png",
+                                          fit: BoxFit.fill,
+                                        )),
+                      ],
+                    )),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(bottom: 120),
+                    height: 300,
+                    width: 300,
+                    child: Image.asset(
+                      "lib/Assets/11.png",
+                      fit: BoxFit.fill,
+                    ),
+                  ),
+                ],
+              );
+            },
+          ),
         ],
       ),
     );
